d5c98441068e32e9825b37dff5c3c8ce0296623f
matthew.lau <matthew.lau@credera.co.uk>
2023-12-18 09:06:59 +0000

    chore: update scheduler to trigger tf workflow

--- deploy/app/scheduler.tf
+++ deploy/app/scheduler.tf
@@ -1,7 +1,7 @@
 resource "google_cloud_scheduler_job" "ingest" {
   name             = "${var.application_name}-scheduled-ingest"
   description      = "${var.application_name} scheduled ingest"
-  schedule         = "*/10 * * * *"
+  schedule         = "*/5 * * * *"
   attempt_deadline = "320s"
   region           = var.region
   project          = var.project
